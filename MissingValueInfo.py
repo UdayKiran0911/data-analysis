67893f500349a3225d82a9308cab4a731227721a
Uday Kiran <98771149+UdayKiran0911@users.noreply.github.com>
2022-06-01 14:59:54 +0530

    Update MissingValueInfo.py

--- MissingValueInfo.py
+++ MissingValueInfo.py
@@ -1,11 +1,13 @@
 # Missing Value Information
 
 def MissingValueInfo(data):
-    missdf=pd.DataFrame(columns=["dtype","#NAs","NA_pcnt", "head"], index=data.columns[data.isnull().any()])
+    missdf=pd.DataFrame(columns=["dtype","#NAs","NA_byTNA_pct","NA_byRows_pct", "head"], 
+                        index=data.columns[data.isnull().any()])
     totalnas = data.isnull().sum().sum() 
     for i in missdf.index:
         missdf.loc[i,"dtype"] = data[i].dtype
         missdf.loc[i,"#NAs"] = data[i].isnull().sum()
-        missdf.loc[i,"NA_pcnt"] = round((data[i].isnull().sum()/totalnas)*100, 2)
+        missdf.loc[i,"NA_byTNA_pct"] = round((data[i].isnull().sum()/totalnas)*100, 2)
+        missdf.loc[i,"NA_byRows_pct"] = round((data[i].isnull().sum()/data.shape[0])*100, 2)
         missdf.loc[i,"head"] = [data[i][0:5]]
     return missdf
